09dc09eab11b57e0447925b23283b7f7098c8338
farhan 27007 <muhammadfarhan27007@gmail.com>
2020-11-08 06:41:37 +0700

    frist commit

---
+++ assets/template/tabel-top-status-surat.php
@@ -0,0 +1,66 @@
+<div class="card">
+  <div class="card-header card-header-icon card-header-rose">
+    <div class="card-icon">
+      <i class="material-icons">mail_outline</i>
+    </div>
+    <h4 class="card-title title-per-section">
+      Status surat
+    </h4>
+  </div>
+  <div class="card-body">
+    <div class="material-datatables">
+      <table id="datatables3" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+        <thead>
+          <tr>
+            <th>No Agenda</th>
+            <th>Jenis surat</th>
+            <th>Tahun</th>
+            <th>Nomor surat</th>
+            <th>Tanggal surat</th>
+            <th>Perihal</th>
+            <th>Surat Dari</th>
+            <th>Isi Disposisi</th>
+            <th>Diteruskan kepada</th>
+            <th>Status surat</th>
+            <th>Scan surat</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <!-- kodisi surat dalam proses -->
+          <tr>
+            <td>1248</td>
+            <td>Segera</td>
+            <td>2016</td>
+            <td>800/1263/2026</td>
+            <td>27-10-2016</td>
+            <td>Permohonan data PNS</td>
+            <td>Badan kepegawaian daerah</td>
+            <td></td>
+            <td></td>
+            <td><div class="alert alert-primary">surat sedang dalam proses</div></td>
+            <td><a href="#">EKD Permohonan Data PNS</a></td>   
+          </tr>
+          <!-- kodisi surat dalam proses -->
+
+          <!-- kodisi surat dibaca -->
+          <tr>
+            <td>1248</td>
+            <td>Segera</td>
+            <td>2016</td>
+            <td>800/1263/2026</td>
+            <td>27-10-2016</td>
+            <td>Permohonan data PNS</td>
+            <td>Badan kepegawaian daerah</td>
+            <td></td>
+            <td></td>    
+            <td><div class="alert alert-success">surat dibaca</div></td>
+            <td><a href="#">EKD Permohonan Data PNS</a></td>
+          </tr>                    
+          <!-- kodisi surat dibaca -->
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
