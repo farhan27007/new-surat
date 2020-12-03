322490d279d3298c6e1f54387117b2ac50e4f0dc
farhan 27007 <muhammadfarhan27007@gmail.com>
2020-12-03 21:34:24 +0700

    penambahan landing page

---
+++ home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portal Resmi Pemerintahan Kabupaten Lampung Selatan</title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <link rel="canonical" href="https://www.creative-tim.com/product/material-dashboard-pro" />
+
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+
+  <!-- CSS Files -->
+
+  <link href="assets/css/material-dashboard.min1c51.css?v=2.1.2" rel="stylesheet" />
+  <link href="assets/demo/demo.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+  <div class="wrapper wrapper-full-page">
+    <div class="page-header login-page header-filter login-property">
+      <div class="container mt-4 pt-3">
+
+        <div class="col-12 text-center">
+          <p>
+            <a class="navbar-brand" href="javascript:;">
+              <img src="assets/img/logo-lamsel.png" height="100" alt="">
+            </a>
+          </p>
+          <h4 class="font-weight-bold">PEMERINTAH DAERAH</h4>
+          <h5 class="font-weight-bold">KABUPATEN LAMPUNG SELATAN</h5>
+          <p class="h6 font-weight-light">Portal Resmi Pemerintah Kabupaten Lampung Selatan</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 col-sm-6">
+            <form class="form" method="" action="#">
+              <div class="card card-login card-hidden">
+                <div class="card-body p-3 text-center">
+                  <p class="mb-0 icon-app-lp"><i class="material-icons">people_outline</i></p>
+                  <p class="card-description h3 font-weight-bold mt-2">Aplikasi</p>
+                  <p class="link-app-lp"><a href="" >Data Kepegawaian</a></p>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div class="col-lg-6 col-md-6 col-sm-6">
+            <form class="form" method="" action="#">
+              <div class="card card-login card-hidden">
+                <div class="card-body p-3 text-center">
+                  <p class="mb-0 icon-app-lp"><i class="material-icons">mail_outline</i></p>
+                  <p class="card-description h3 font-weight-bold mt-2">Aplikasi</p>
+                  <p class="link-app-lp"><a href="" >Pengelolaan Surat</a></p>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <script src="assets/js/material-dashboard.min1c51.js?v=2.1.2" type="text/javascript"></script>
+  
+  <script>
+    $(document).ready(function() {
+      md.checkFullPageBackgroundImage();
+      setTimeout(function() {
+        // after 1000 ms we add the class animated to the login/register card
+        $('.card').removeClass('card-hidden');
+      }, 700);
+    });
+  </script>
+</body>
+</html>
